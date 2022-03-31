d5298006e903744a42a3a8a300072059acddd790
pq43 <51523958+pq43@users.noreply.github.com>
2022-03-31 06:27:29 +0000

    added dynamic array, memory allocation helpers

--- main.c
+++ main.c
@@ -1,6 +1,12 @@
-#include <stdio.h>
+#include "common.h"
+#include "chunk.h"
+#include "debug.h"
 
-int main(void) {
-  printf("Hello World Again\n");
+int main(int argc, const char* argv[]) {
+	Chunk chunk;
+	initChunk(&chunk);
+	writeChunk(&chunk, OP_RETURN);
+	disassembleChunk(&chunk, "test chunk");
+	freeChunk(&chunk);
   return 0;
 }
