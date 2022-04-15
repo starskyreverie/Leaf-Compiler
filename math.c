8eac046279ed5533d2d537c78d6d6dd9e2db6058
pq43 <51523958+pq43@users.noreply.github.com>
2022-04-15 19:17:58 +0000

    math add

---
+++ math.c
@@ -0,0 +1,5 @@
+#include <math.h>
+
+double logBaseXOf(int x, int num) {
+	return log(num)/log(x);
+}
---
+++ math.h
@@ -0,0 +1,18 @@
+#ifndef clox_math_h
+#define clox_math_h
+
+#include <math.h>
+
+int logBaseXOf(int x, int num);
+//int sin(int x);
+//int cos(int x);
+//int tan(int x);
+//int sqrt(int x);
+//int abs(int x);
+//int floor(int x);
+//int ceil(int x);
+int solveAlgExp(char* s);
+int derivative(char* s);
+int integral(char* s);
+
+#endif
